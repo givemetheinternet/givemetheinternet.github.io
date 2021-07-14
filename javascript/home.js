a3eff16fcc6e18d9c6cd1ac191f0f0bf35d3af99
Eleanor Reich <ereich@hubspot.com>
2021-07-13 21:26:18 -0400

    Initial commit

---
+++ javascript/home.js
@@ -0,0 +1,12 @@
+
+document.getElementById("button-1").onclick = function () {
+  location.href = "./pages/about.html";
+};
+
+document.getElementById("button-2").onclick = function () {
+  location.href = "https://www.w3schools.com/jsref/met_win_alert.asp";
+};
+
+document.getElementById("button-3").onclick = function () {
+  location.href = "https://hackr.io/blog/javascript-projects";
+};
