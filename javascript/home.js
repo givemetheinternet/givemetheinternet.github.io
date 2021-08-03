be54e79eab7c695dbb424111c4da1b0719a438ca
Robert McEneaney <rmceneaney@hubspot.com>
2021-08-03 16:23:31 -0400

    Better ux

--- javascript/home.js
+++ javascript/home.js
@@ -1,31 +1,32 @@
+// Make a guess if the user hits 'Enter' from the text box
+document.getElementById("textGuess")
+    .addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        document.getElementById("buttonGuess").click();
+    }
+});
 
-document.getElementById("button-1").onclick = function () {
-  location.href = "./pages/about.html";
-};
-
-document.getElementById("button-2").onclick = function () {
-  alert("Hello world!");
-};
-
-document.getElementById("button-3").onclick = function () {
-  document.getElementById("content").style.color = "blue";
-};
-
+// Think of a random number
 var myNumber = Math.ceil(Math.random() * 100);
 var numGuesses = 0;
 
+// Compare the user's guess to the number I thought of
 function guessNumber(userGuess) {
   numGuesses++;
   if (userGuess <  myNumber) {
     alert("That number is too low, try again!");
+    document.getElementById("textGuess").select();
   } else if (userGuess > myNumber) {
     alert("That number is too high, try again!");
+    document.getElementById("textGuess").select();
   } else {
     alert("You got it in " + numGuesses + " guesses!!! The number was " + myNumber);
     showSurprise();
   }
+}
 
-  function showSurprise() {
-    document.getElementById("surprise").style.visibility="visible";
-  }
+// Unhide the video once they guess the number
+function showSurprise() {
+  document.getElementById("surprise").style.visibility="visible";
 }
